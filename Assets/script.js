3c614f7efc21df8d27a8551194773fec87225f91
Wendy Fgigueroa <camomila444@gmail>
2020-12-19 12:29:34 -0500

    start

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,6 @@
+
+/*---swiper 1---*/
+
 var swiper = new Swiper('.swiper-container', {
     effect: 'coverflow',
     grabCursor: true,
@@ -9,6 +12,22 @@ var swiper = new Swiper('.swiper-container', {
       depth: 100,
       modifier: 1,
       slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });
+
+/*----swiper 2----*/
+
+var swiper = new Swiper('.swiper-container', {
+    effect: 'cube',
+    grabCursor: true,
+    cubeEffect: {
+      shadow: true,
+      slideShadows: true,
+      shadowOffset: 20,
+      shadowScale: 0.94,
     },
     pagination: {
       el: '.swiper-pagination',
