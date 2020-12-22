44d12ab9c8f19fd96aea0a352485e0053794d90d
Wendy Fgigueroa <camomila444@gmail>
2020-12-22 15:28:43 -0500

    updating navbar and search

--- Assets/script.js
+++ Assets/script.js
@@ -22,26 +22,14 @@ var swiper = new Swiper('.swiper-container', {
     pagination: {
       el: '.swiper-pagination',
     },
-       
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+
+    },
+
    });
 
   
 /*----swiper 2----*/
 
-// var swiper = new Swiper('.swiper-container', {
-//     effect: 'cube',
-//     grabCursor: true,
-//     cubeEffect: {
-//       shadow: true,
-//       slideShadows: true,
-//       shadowOffset: 20,
-//       shadowScale: 0.94,
-//     },
-//     pagination: {
-//       el: '.swiper-pagination',
-//     },
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
-//   });
